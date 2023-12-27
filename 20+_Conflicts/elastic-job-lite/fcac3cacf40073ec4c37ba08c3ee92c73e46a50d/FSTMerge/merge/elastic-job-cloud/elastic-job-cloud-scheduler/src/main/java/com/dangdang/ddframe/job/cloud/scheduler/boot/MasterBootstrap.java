c07835c8e039bada476be48af2affb525e601592
Bowen Shen <bowenshe@vt.edu>
2023-12-27 12:09:54 -0500

    Initial commit without evaluation result

---
+++ 20+_Conflicts/elastic-job-lite/fcac3cacf40073ec4c37ba08c3ee92c73e46a50d/FSTMerge/merge/elastic-job-cloud/elastic-job-cloud-scheduler/src/main/java/com/dangdang/ddframe/job/cloud/scheduler/boot/MasterBootstrap.java
@@ -0,0 +1,309 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package com.dangdang.ddframe.job.cloud.scheduler.boot; 
+
+import com.dangdang.ddframe.job.cloud.scheduler.boot.env.BootstrapEnvironment; 
+import com.dangdang.ddframe.job.cloud.scheduler.boot.env.MesosConfiguration; 
+import com.dangdang.ddframe.job.cloud.scheduler.config.CloudJobConfigurationListener; 
+import com.dangdang.ddframe.job.cloud.scheduler.config.ConfigurationNode; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.FacadeService; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.LeasesQueue; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.SchedulerEngine; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.StatisticsProcessor; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.TaskLaunchProcessor; 
+import com.dangdang.ddframe.job.cloud.scheduler.producer.ProducerManager; 
+import com.dangdang.ddframe.job.cloud.scheduler.producer.ProducerManagerFactory; 
+import com.dangdang.ddframe.job.cloud.scheduler.restful.CloudJobRestfulApi; 
+import com.dangdang.ddframe.job.cloud.scheduler.statistics.StatisticManager; 
+import com.dangdang.ddframe.job.event.JobEventBus; 
+import com.dangdang.ddframe.job.event.rdb.JobEventRdbConfiguration; 
+import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter; 
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter; 
+import com.dangdang.ddframe.job.restful.RestfulServer; 
+import com.google.common.base.Optional; 
+import com.netflix.fenzo.TaskScheduler; 
+import com.netflix.fenzo.VirtualMachineLease; 
+import com.netflix.fenzo.functions.Action1; 
+import lombok.extern.slf4j.Slf4j; 
+import org.apache.curator.framework.recipes.cache.TreeCache; 
+import org.apache.mesos.MesosSchedulerDriver; 
+import org.apache.mesos.Protos; 
+import org.apache.mesos.SchedulerDriver; 
+import com.dangdang.ddframe.job.event.rdb.JobEventRdbSearch; 
+import com.dangdang.ddframe.job.cloud.scheduler.ha.FrameworkIDService; 
+import com.dangdang.ddframe.job.cloud.scheduler.ha.ReconcileScheduledService; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.StatisticsScheduledService; 
+import com.dangdang.ddframe.job.cloud.scheduler.mesos.TaskLaunchScheduledService; 
+import com.google.common.util.concurrent.Service; 
+
+import java.util.concurrent.Executors; 
+
+/**
+ * Mesos框架启动器.
+ *
+ * @author zhangliang
+ * @author gaohongtao
+ */
+@Slf4j
+public final  class  MasterBootstrap {
+	
+    
+    private static final String WEBAPP_PATH = "webapp/";
+
+	
+    
+    private final BootstrapEnvironment env;
+
+	
+    
+    private final CoordinatorRegistryCenter regCenter;
+
+	
+    
+    private SchedulerDriver schedulerDriver;
+
+	
+    
+    private final RestfulServer restfulServer;
+
+	
+    
+    public MasterBootstrap() {
+        env = BootstrapEnvironment.getInstance();
+        regCenter = getRegistryCenter();
+<<<<<<< /home/ppp/Research_Projects/Merge_Conflicts/Resource/Paper/bowen-icsme-2023-data/ConflictBench/20_Conflicts/workspace/fstmerge_tmp1703457035498/fstmerge_var1_1680154089102162154
+        facadeService = new FacadeService(regCenter);
+        Optional<JobEventRdbSearch> jobEventRdbSearch = Optional.absent();
+        if (env.getJobEventRdbConfiguration().isPresent()) {
+            jobEventRdbSearch = Optional.of(new JobEventRdbSearch(env.getJobEventRdbConfiguration().get().getDataSource()));
+        }
+        statisticManager = StatisticManager.getInstance(regCenter, env.getJobEventRdbConfiguration());
+        restfulServer = new RestfulServer(env.getRestfulServerConfiguration().getPort());
+        frameworkIDService = new FrameworkIDService(regCenter);
+        CloudJobRestfulApi.init(regCenter, jobEventRdbSearch);
+=======
+        LeasesQueue leasesQueue = new LeasesQueue();
+        final FacadeService facadeService = new FacadeService(regCenter);
+        TaskScheduler taskScheduler = getTaskScheduler();
+        JobEventBus jobEventBus = getJobEventBus();
+        final StatisticManager statisticManager = StatisticManager.getInstance(regCenter, env.getJobEventRdbConfiguration());
+        statisticManager.startup();
+        schedulerDriver = getSchedulerDriver(leasesQueue, taskScheduler, facadeService, jobEventBus, statisticManager);
+        restfulServer = new RestfulServer(env.getRestfulServerConfiguration().getPort());
+        CloudJobRestfulApi.init(schedulerDriver, regCenter);
+        initConfigurationListener();
+        final ProducerManager producerManager = ProducerManagerFactory.getInstance(schedulerDriver, regCenter);
+        producerManager.startup();
+        new Thread(new TaskLaunchProcessor(leasesQueue, schedulerDriver, taskScheduler, facadeService, jobEventBus), "task-launch-processor-" + Thread.currentThread().getId()).start();
+        new Thread(new StatisticsProcessor(), "statistics-processor-" + Thread.currentThread().getId()).start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            
+            @Override
+            public void run() {
+                facadeService.stop();
+                producerManager.shutdown();
+                statisticManager.shutdown();
+            }
+        });
+>>>>>>> /home/ppp/Research_Projects/Merge_Conflicts/Resource/Paper/bowen-icsme-2023-data/ConflictBench/20_Conflicts/workspace/fstmerge_tmp1703457035498/fstmerge_var2_3368207892153920288
+    }
+
+
+	
+    
+    private CoordinatorRegistryCenter getRegistryCenter() {
+        CoordinatorRegistryCenter result = new ZookeeperRegistryCenter(env.getZookeeperConfiguration());
+        result.init();
+        return result;
+    }
+
+
+	
+    
+    
+
+	
+    
+    private TaskScheduler getTaskScheduler() {
+        return new TaskScheduler.Builder()
+                .withLeaseOfferExpirySecs(1000000000L)
+                .withLeaseRejectAction(new Action1<VirtualMachineLease>() {
+                    
+                    @Override
+                    public void call(final VirtualMachineLease lease) {
+                        log.warn("Declining offer on '{}'", lease.hostname());
+                        schedulerDriver.declineOffer(lease.getOffer().getId());
+                    }
+                }).build();
+    }
+
+
+	
+    
+    
+
+	
+    
+    private JobEventBus getJobEventBus() {
+        Optional<JobEventRdbConfiguration> rdbConfig = env.getJobEventRdbConfiguration();
+        if (rdbConfig.isPresent()) {
+            return new JobEventBus(rdbConfig.get());
+        }
+        return new JobEventBus();
+    }
+
+
+	
+    
+    /**
+     * 以守护进程方式启动.
+     * 
+     * @return 框架运行状态
+     * @throws Exception 运行时异常
+     */
+    
+
+	
+    
+    /**
+     * 停止运行.
+     * 
+     * @param status 框架运行状态
+     * @return 是否正常停止
+     * @throws Exception 运行时异常
+     */
+    
+
+	
+    
+    private static final double ONE_WEEK_TIMEOUT = 60 * 60 * 24 * 7;
+
+	
+    
+    private final FacadeService facadeService;
+
+	
+    
+    private final StatisticManager statisticManager;
+
+	
+    
+    private final FrameworkIDService frameworkIDService;
+
+	
+    
+    private ProducerManager producerManager;
+
+	
+    
+    private CloudJobConfigurationListener cloudJobConfigurationListener;
+
+	
+    
+    private Service taskLaunchScheduledService;
+
+	
+    
+    private Service statisticsScheduledService;
+
+	
+    
+    private Service reconcileScheduledService;
+
+	
+    
+    /**
+     * 以守护进程方式启动.
+     * 
+     * @throws Exception 启动异常
+     */
+    public void start() throws Exception {
+        LeasesQueue leasesQueue = new LeasesQueue();
+        TaskScheduler taskScheduler = getTaskScheduler();
+        JobEventBus jobEventBus = getJobEventBus();
+        schedulerDriver = getSchedulerDriver(leasesQueue, taskScheduler, jobEventBus);
+        producerManager = new ProducerManager(schedulerDriver, regCenter);
+        producerManager.startup();
+        CloudJobRestfulApi.setContext(schedulerDriver, producerManager);
+        log.info("Elastic Job: Add configuration listener");
+        cloudJobConfigurationListener =  new CloudJobConfigurationListener(schedulerDriver, producerManager, regCenter);
+        getCache().getListenable().addListener(cloudJobConfigurationListener, Executors.newSingleThreadExecutor());
+        taskLaunchScheduledService = new TaskLaunchScheduledService(leasesQueue, schedulerDriver, taskScheduler, facadeService, jobEventBus).startAsync();
+        statisticsScheduledService = new StatisticsScheduledService(regCenter).startAsync();
+        reconcileScheduledService =  ReconcileScheduledService.builder().facadeService(facadeService).scheduler(schedulerDriver).build().startAsync();
+        statisticManager.startup();
+        restfulServer.start(CloudJobRestfulApi.class.getPackage().getName(), WEBAPP_PATH);
+        schedulerDriver.start();
+    }
+
+	
+    
+    private SchedulerDriver getSchedulerDriver(final LeasesQueue leasesQueue, final TaskScheduler taskScheduler, final JobEventBus jobEventBus) {
+        MesosConfiguration mesosConfig = env.getMesosConfiguration();
+        Optional<String> frameworkIDOptional = frameworkIDService.fetch();
+        Protos.FrameworkInfo.Builder builder = Protos.FrameworkInfo.newBuilder();
+        if (frameworkIDOptional.isPresent()) {
+            builder.setId(Protos.FrameworkID.newBuilder().setValue(frameworkIDOptional.get()).build());
+        }
+        Protos.FrameworkInfo frameworkInfo = builder.setUser(mesosConfig.getUser()).setName(MesosConfiguration.FRAMEWORK_NAME)
+                        .setHostname(mesosConfig.getHostname()).setFailoverTimeout(ONE_WEEK_TIMEOUT).build();
+        return new MesosSchedulerDriver(new SchedulerEngine(leasesQueue, taskScheduler, facadeService, jobEventBus, frameworkIDService, statisticManager), frameworkInfo, mesosConfig.getUrl());
+    }
+
+	
+    
+    private TreeCache getCache() {
+        TreeCache result = (TreeCache) regCenter.getRawCache(ConfigurationNode.ROOT);
+        if (null != result) {
+            return result;
+        }
+        regCenter.addCacheData(ConfigurationNode.ROOT);
+        return (TreeCache) regCenter.getRawCache(ConfigurationNode.ROOT);
+    }
+
+	
+    
+    /**
+     * 停止运行.
+     */
+    public synchronized void stop() {
+        if (null != taskLaunchScheduledService) {
+            taskLaunchScheduledService.stopAsync();
+        }
+        if (null != statisticsScheduledService) {
+            statisticsScheduledService.stopAsync();
+        }
+        if (null != reconcileScheduledService) {
+            reconcileScheduledService.stopAsync();
+        }
+        restfulServer.stop();
+        statisticManager.shutdown();
+        if (null != cloudJobConfigurationListener) {
+            log.info("Elastic Job: Remove configuration listener");
+            getCache().getListenable().removeListener(cloudJobConfigurationListener);
+        }
+        if (null != producerManager) {
+            producerManager.shutdown();
+        }
+        if (null != schedulerDriver) {
+            schedulerDriver.stop(true);
+        }
+    }
+
+
+}
