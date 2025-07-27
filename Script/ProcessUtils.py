e91453f2d47a149bedf78316867b4ee822502ebc
gqqnbig <gqqnb2005@gmail.com>
2025-07-27 16:55:49 +0200

    Let callers decode stdout

--- Script/ProcessUtils.py
+++ Script/ProcessUtils.py
@@ -6,18 +6,22 @@ class ProcessException(Exception):
 		self.message = message
 
 
-def runProcess(cmd, timeout) -> str:
+def runProcess(cmd, timeout) -> bytes:
 	"""
 
 	:param cmd:
 	:param timeout:
 	:return: stdout
 	"""
-	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	try:
 		outs, errs = proc.communicate(timeout=timeout)
 		if proc.returncode != 0:
+			errs = errs.decode('utf-8', errors='ignore')
+			if len(errs) > 500:
+				errs = f'Error message has {len(errs)} characters.'
 			raise ProcessException("Fail to run '" + cmd + "' in shell: " + errs)
+
 		return outs
 	except subprocess.TimeoutExpired:
 		# Terminate the unfinished process
